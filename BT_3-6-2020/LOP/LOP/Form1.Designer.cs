550c1aa28c25d2a18c7a0965b16ccc140a776820
VoLeKhanhDuy <vlkduy1999@gmail.com>
2020-06-03 15:02:41 +0700

    Update BT kncsdl bang LOP

---
+++ BT_3-6-2020/LOP/LOP/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace LOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Them = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_MaLop = new System.Windows.Forms.TextBox();
+            this.dgv_lop = new System.Windows.Forms.DataGridView();
+            this.txt_TenLop = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Xoa = new System.Windows.Forms.Button();
+            this.btn_Sua = new System.Windows.Forms.Button();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Luu = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_lop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Them
+            // 
+            this.btn_Them.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Them.Location = new System.Drawing.Point(54, 271);
+            this.btn_Them.Name = "btn_Them";
+            this.btn_Them.Size = new System.Drawing.Size(75, 23);
+            this.btn_Them.TabIndex = 0;
+            this.btn_Them.Text = "Thêm";
+            this.btn_Them.UseVisualStyleBackColor = true;
+            this.btn_Them.Click += new System.EventHandler(this.btn_Them_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã lớp:";
+            // 
+            // txt_MaLop
+            // 
+            this.txt_MaLop.Location = new System.Drawing.Point(99, 64);
+            this.txt_MaLop.Name = "txt_MaLop";
+            this.txt_MaLop.Size = new System.Drawing.Size(111, 20);
+            this.txt_MaLop.TabIndex = 2;
+            // 
+            // dgv_lop
+            // 
+            this.dgv_lop.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_lop.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dgv_lop.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_lop.Location = new System.Drawing.Point(54, 102);
+            this.dgv_lop.Name = "dgv_lop";
+            this.dgv_lop.Size = new System.Drawing.Size(430, 150);
+            this.dgv_lop.TabIndex = 3;
+            this.dgv_lop.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_lop_CellContentClick);
+            // 
+            // txt_TenLop
+            // 
+            this.txt_TenLop.Location = new System.Drawing.Point(299, 64);
+            this.txt_TenLop.Name = "txt_TenLop";
+            this.txt_TenLop.Size = new System.Drawing.Size(185, 20);
+            this.txt_TenLop.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(247, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tên lớp:";
+            // 
+            // btn_Xoa
+            // 
+            this.btn_Xoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Xoa.Location = new System.Drawing.Point(234, 271);
+            this.btn_Xoa.Name = "btn_Xoa";
+            this.btn_Xoa.Size = new System.Drawing.Size(75, 23);
+            this.btn_Xoa.TabIndex = 6;
+            this.btn_Xoa.Text = "Xóa";
+            this.btn_Xoa.UseVisualStyleBackColor = true;
+            this.btn_Xoa.Click += new System.EventHandler(this.btn_Xoa_Click);
+            // 
+            // btn_Sua
+            // 
+            this.btn_Sua.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Sua.Location = new System.Drawing.Point(144, 271);
+            this.btn_Sua.Name = "btn_Sua";
+            this.btn_Sua.Size = new System.Drawing.Size(75, 23);
+            this.btn_Sua.TabIndex = 7;
+            this.btn_Sua.Text = "Sửa";
+            this.btn_Sua.UseVisualStyleBackColor = true;
+            this.btn_Sua.Click += new System.EventHandler(this.btn_Sua_Click);
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Thoat.Location = new System.Drawing.Point(322, 271);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(75, 23);
+            this.btn_Thoat.TabIndex = 9;
+            this.btn_Thoat.Text = "Thoát";
+            this.btn_Thoat.UseVisualStyleBackColor = true;
+            this.btn_Thoat.Click += new System.EventHandler(this.btn_Thoat_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(105, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(319, 26);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "CẬP NHẬT THÔNG TIN LỚP";
+            // 
+            // btn_Luu
+            // 
+            this.btn_Luu.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Luu.Location = new System.Drawing.Point(409, 271);
+            this.btn_Luu.Name = "btn_Luu";
+            this.btn_Luu.Size = new System.Drawing.Size(75, 23);
+            this.btn_Luu.TabIndex = 11;
+            this.btn_Luu.Text = "Lưu";
+            this.btn_Luu.UseVisualStyleBackColor = true;
+            this.btn_Luu.Click += new System.EventHandler(this.btn_Luu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 307);
+            this.Controls.Add(this.btn_Luu);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_Thoat);
+            this.Controls.Add(this.btn_Sua);
+            this.Controls.Add(this.btn_Xoa);
+            this.Controls.Add(this.txt_TenLop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgv_lop);
+            this.Controls.Add(this.txt_MaLop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Them);
+            this.Name = "Form1";
+            this.Text = "Bảng cập nhật thông tin lớp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_lop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Them;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_MaLop;
+        private System.Windows.Forms.DataGridView dgv_lop;
+        private System.Windows.Forms.TextBox txt_TenLop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Xoa;
+        private System.Windows.Forms.Button btn_Sua;
+        private System.Windows.Forms.Button btn_Thoat;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Luu;
+    }
+}
+
---
+++ BT_3-6-2020/LOP/LOP/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOP
+{
+    public partial class Form1 : Form
+    {
+        Ket_Noi_CSDL csdl = new Ket_Noi_CSDL();
+
+        public Form1()
+        {
+            InitializeComponent();
+            hienThiDuLieu();
+        }
+
+        public void hienThiDuLieu()
+        {
+            DataTable bangLop = new DataTable();
+            String sql = "select * from TB_LOP";
+            bangLop = csdl.LayBang(sql);
+            dgv_lop.DataSource = bangLop;
+        }
+        
+
+        private void btn_Thoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgv_lop_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            txt_MaLop.Text = dgv_lop.Rows[index].Cells[0].Value.ToString();
+            txt_TenLop.Text = dgv_lop.Rows[index].Cells[1].Value.ToString();
+            txt_MaLop.ReadOnly = true;
+        }
+
+        private void btn_Them_Click(object sender, EventArgs e)
+        {
+            String maLop = txt_MaLop.Text;
+            String tenLop = txt_TenLop.Text;
+
+            String sql = "insert into TB_LOP values('" + maLop + "', N'" + tenLop + "')";
+
+            if (csdl.CapnhatCSDL(sql))
+            {
+                MessageBox.Show("Thêm thành công!");
+            }
+            else
+            {
+                MessageBox.Show("Thêm thất bại!");
+            }
+            hienThiDuLieu();
+        }
+
+        private void btn_Sua_Click(object sender, EventArgs e)
+        {
+            String maLop = txt_MaLop.Text;
+            String tenLop = txt_TenLop.Text;
+
+            String sql = "update TB_LOP set TenLop = N'" + tenLop + "' where MaLop = '" + maLop + "'";
+
+            if (csdl.CapnhatCSDL(sql))
+            {
+                MessageBox.Show("Sửa thành công!");
+            }
+            else
+            {
+                MessageBox.Show("Sửa thất bại!");
+            }
+            hienThiDuLieu();
+        }
+
+        private void btn_Xoa_Click(object sender, EventArgs e)
+        {
+            String maLop = txt_MaLop.Text;
+        
+
+            String sql = "delete TB_LOP where MaLop = '" + maLop + "'";
+            if (csdl.CapnhatCSDL(sql))
+            {
+                MessageBox.Show("Xóa thành công!");
+            }
+            else
+            {
+                MessageBox.Show("Xóa thất bại!");
+            }
+            hienThiDuLieu();
+        }
+
+        private void btn_Luu_Click(object sender, EventArgs e)
+        {
+            String maLop = txt_MaLop.Text;
+            String tenLop = txt_TenLop.Text;
+
+            String sql = "insert into TB_LOP values('" + maLop + "', N'" + tenLop + "')";
+            if (csdl.CapnhatCSDL(sql))
+            {
+                MessageBox.Show("Lưu thành công!");
+            }
+            else
+            {
+                MessageBox.Show("Lưu thất bại!");
+            }
+            hienThiDuLieu();
+        }
+    }
+}
